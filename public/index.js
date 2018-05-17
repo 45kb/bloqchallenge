d601557054d78643eecd04e0ded83e71625fc681
45kb <filippo.oretti@gmail.com>
2018-05-17 12:10:12 +0200

    almost done need javascript for UI

---
+++ public/index.js
@@ -0,0 +1,11 @@
+(function () {
+
+var signinSection = document.getElementById('signin-section')
+  , loginSection = document.getElementById('login-section')
+  , OpenLoginForm = function OpenLoginForm() {
+
+  }
+  , OpenSigninForm = function OpenSigninForm() {
+
+  };
+})();
